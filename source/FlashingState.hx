d6edb7d3d8b03096b44a4852a7c613bfd6738b27
NT712 <97837270+NT712@users.noreply.github.com>
2022-03-02 19:41:15 +0700

    Update (Credits)

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,13 +24,9 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!
-			Random Engine by randomxd/n
-			Psych Port by Saw and Sirox",
+			"Port by NT846
+			PE Source Code by Saw
+			Custom Engine (Random Engine) by a random.",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
